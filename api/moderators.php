bb9831b388918705f6aeee9182ed647f111c854c
judemont <julien@rmbi.ch>
2023-04-12 13:25:38 +0200

    Fix typing error in moderator.php

--- api/moderators.php
+++ api/moderators.php
@@ -1,6 +1,6 @@
 <?php
     header('Content-Type: ' . JSON_CONTENT_TYPE);
     $moderators_pk = ["157", "150", "181", "183"];
-    echo json_encode($moderators);
+    echo json_encode($moderators_pk);
 
 ?>
