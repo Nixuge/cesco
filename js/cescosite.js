568d4b17880e77e320e63f6898cbb99f2f47addd
judemont <julien@rmbi.ch>
2023-04-12 11:37:13 +0200

    just

--- js/cescosite.js
+++ js/cescosite.js
@@ -2,3 +2,8 @@ import Post from "../components/Post.js"
 import decodeEntity from "../utils/decodeEntity.js"   
 import getAllParents from "../utils/getAllParents.js"
 
+const postApi
+
+async function getPostsData(){
+    $.get
+}
