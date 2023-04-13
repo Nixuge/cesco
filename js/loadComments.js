473ed75f2fcd7c132b0166fa0f873c8b285edb23
judemont <julien@rmbi.ch>
2023-04-13 22:07:38 +0200

    `function loadComments(article_pk)`

--- js/loadComments.js
+++ js/loadComments.js
@@ -6,7 +6,29 @@ const artZone = document.getElementById("artZone");
 const commentEmplacement = document.getElementById("comsjs");
 const data = getPostsData();
 
+function loadComments(article_pk)
+{
+    commentEmplacement.innerHTML = ''
+    let art_index = Object.keys(data).find(key => data[key].ARTICLES_PK == article_pk)
+    sendButton.addEventListener("mouseup", function(e) {
+        sendComData(article_pk);
+        loadComments(article_pk)
+        
+    })
+    console.log(art_index)
+    if(data[art_index].comments != "undefined"){
+        const comments = data[art_index].comments
+        for (let ii = 0; ii < comments.length; ii++) {
+            const com = comments[ii];
+            const comAuthor = "username"
+            const comDate = com.dat
+            const comContent = com.content
+            const comHtml = Comment(comAuthor, comDate, comContent)
+            commentEmplacement.appendChild(comHtml)
+        }
+    }
 
+}
 artZone.addEventListener(
 "mouseover",
 (event) => {
@@ -18,25 +40,7 @@ artZone.addEventListener(
         let element = parents[i]
         let infos = element.id.split("#")
         if(infos[0] == "art"){
-            commentEmplacement.innerHTML = ''
-            let art_index = Object.keys(data).find(key => data[key].ARTICLES_PK == infos[1])
-            sendButton.addEventListener("mouseup", function(e) {
-                sendComData(infos[1]);
-                sendButton.removeEventListener("mouseup", sendComHandler);
-            })
-            console.log(art_index)
-            if(data[art_index].comments != "undefined"){
-                const comments = data[art_index].comments
-                for (let ii = 0; ii < comments.length; ii++) {
-                    const com = comments[ii];
-                    const comAuthor = "username"
-                    const comDate = com.dat
-                    const comContent = com.content
-                    const comHtml = Comment(comAuthor, comDate, comContent)
-                    commentEmplacement.appendChild(comHtml)
-                }
-            }
-
+            loadComments(infos[1])
         }
         
 
