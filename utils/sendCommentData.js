42f946e7031b6e21e551f7949857c55edfb4875f
judemont <julien@rmbi.ch>
2023-04-13 19:51:09 +0200

    sendCommentData.js

---
+++ utils/sendCommentData.js
@@ -0,0 +1,42 @@
+function sendComData(art_pk)
+{
+
+    
+
+    console.log("SEND COM DATAAAAAAAAAAAAAAAAAAAAAAAAA")
+    user_pk = document.getElementById("user_pk").value
+
+
+    if(user_pk == ''){
+        show_connection();
+       
+
+    }else{
+
+
+    var comcontent = document.getElementById("comsContent").value;
+
+
+
+
+   
+   
+
+    $.ajax({
+        type: 'post',
+        url: '../api/sendComment.php',
+        data: {
+            textC:comcontent,
+            articlePK:a_pk,
+           
+        },
+        success: function(response) {
+         
+
+        }
+    });
+    
+        
+     
+    }
+}
