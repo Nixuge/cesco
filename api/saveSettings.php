b9a254812195db4f53272b44412b3d385609b65f
judemont <julien@rmbi.ch>
2023-04-13 11:29:30 +0200

    move saveSettings into `api` dir

---
+++ api/saveSettings.php
@@ -0,0 +1,66 @@
+
+
+<?php
+	
+	include_once("./utils/db.php");
+	include_once("./utils/isBadUsername.php");
+	include_once("./utils/alert.php");
+	session_start();
+	if(isset($_SESSION['user']) == FALSE){
+	//	header('Location: ?page=connection');
+	}
+	$userPK = $_SESSION['userPK'];
+	
+	
+	if (isset($_POST["newPseudo"])) {
+
+	
+		$newPseudo = $conn -> real_escape_string($_POST["newPseudo"]);
+
+		if(isBadUsername($newPseudo)){
+			alert( "ERREUR : Votre pseudo contient des mots interdits");
+
+		}else{
+			$sql = "UPDATE aj_Users SET username = '$newPseudo' WHERE users_PK = '$userPK'";
+			if (mysqli_query($conn, $sql)) {
+				alert("Votre pseudo a été mis a jour.");
+			}else{
+				alert( "Erreur : " . $sql . "<br>" . mysqli_error($conn));
+			}
+		}
+		
+	}
+
+	if($_POST['newPass'] != ""){
+		if (isset($_POST['oldPass']) && isset($_POST['newPass'])) {
+			$OldPasswd = $conn -> real_escape_string($_POST['oldPass']) ; 
+			$NewPasswd = $conn -> real_escape_string($_POST['newPass']) ; 
+			
+
+			$sql = "SELECT username FROM aj_Users WHERE users_PK = '$userPK' AND passwd = MD5(MD5('$OldPasswd'))";
+
+			$result = $conn->query($sql);
+			$row = $result->fetch_assoc();
+			
+			if ($result->num_rows > 0) {
+				//old passwd OK:
+				$sql = "UPDATE aj_Users SET passwd = MD5(MD5('$NewPasswd')) WHERE users_PK = '$userPK'";
+
+				if (mysqli_query($conn, $sql)) {
+					alert("Votre mot de passe a été mis a jour.");
+				}else{
+					alert("Erreur : " . $sql . "<br>" . mysqli_error($conn));
+				}
+
+			}else{
+				alert( "Ancien mot de passe incorrect");
+			}
+		
+	}
+
+	}
+
+	echo "<script>window.location.href='index.php' </script>";
+?>
+
+
---
+++ utils/isBadUsername.php
@@ -0,0 +1,14 @@
+<?php
+    function isBadUsername($sentence)
+    {
+        $bannedWord = array("connard", "pute", "fuck", "sex", "sexy", "connard", "fuck","foutre", "ftg", "ta geul", "nique", "cul", "merde", "couille", "bite", "hitler", "staline", "nazi", "debile", "débile", "con", "débil", "debil");
+
+        for ($i=0; $i<count($bannedWord); $i++) {
+            if(strpos($sentence, $bannedWord[$i]) !== FALSE){
+                return TRUE;
+            }
+        }
+
+    }
+
+?>
