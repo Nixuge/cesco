de0b641c3fa6b2da7ee1490bfbf2ee808bf2804b
judemont <julien@rmbi.ch>
2023-04-16 12:39:57 +0200

    Max post lenght

--- js/editorUpdate.js
+++ js/editorUpdate.js
@@ -1,11 +1,19 @@
 const MAX_POST_LENGHT = 1000
 let content = ""
 let contentLength = 0
+const stringToHtmlElement = document.createElement('div')
 const post_lenght_emp = document.getElementById("postLenght")
-$('#editor')
-.trumbowyg() 
-.on('tbwchange', function(){
-    content = $('#editor').trumbowyg('html').textContent;
-    contentLength = content.length()
-    post_lenght_emp.innerHTML(contentLength + "/" + MAX_POST_LENGHT)
+var editor = $('#editor')
+
+editor.on('keyup', function() {
+    content = $('#editor').trumbowyg('html');
+    stringToHtmlElement.innerHTML = content
+    contentText = stringToHtmlElement.innerText
+    contentLength = contentText.length
+    post_lenght_emp.innerHTML = contentLength + "/" + MAX_POST_LENGHT
+    if(contentLength > MAX_POST_LENGHT)
+    {
+        var truncated = contentText.substr(0, MAX_POST_LENGHT);
+        editor.trumbowyg('html', truncated);
+    }
 });
