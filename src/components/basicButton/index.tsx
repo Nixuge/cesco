d07833839899307ffea5ca6c6e17e60b3b4e56a8
JdM <julien@rmbi.ch>
2023-05-24 14:42:24 +0200

    just for childrens

--- src/components/basicButton/index.tsx
+++ src/components/basicButton/index.tsx
@@ -1,9 +1,16 @@
-import styles from "./basicButton.module.css"
+import React from 'react';
+import styles from './basicButton.module.css';
 
-export default function basicButton({children}: any){
-    return(
-        <button className={styles.buttonbottom}>
-            <p className={styles.buttonbottomtext}>{children}</p>
-        </button>
-    )
+interface BasicButtonProps {
+  children: React.ReactNode;
 }
+
+const BasicButton: React.FC<BasicButtonProps> = ({ children }) => {
+  return (
+    <button className={styles.buttonbottom}>
+      <p className={styles.buttonbottomtext}>{children}</p>
+    </button>
+  );
+};
+
+export default BasicButton;
--- src/components/post/index.tsx
+++ src/components/post/index.tsx
@@ -1,33 +1,34 @@
-import styles from "./post.module.css"
-import { ReactNode } from 'react'
-import basicButton from "../basicButton";
+import React, { ReactNode } from 'react';
+import styles from './post.module.css';
+import BasicButton from '../basicButton';
 
-export interface postProps{
-    children: ReactNode,
-    title: string,
-    author: string,
-    date: string,
-}    
-
-export default function Post({children, title, author, date}: postProps){
-
-    return(
-        <div className={styles.post}>
-            <div className={styles.infopost}>
-                <div className={styles.namedatepost}>
-                    <p className={styles.namepost}>{author}</p>
-                    <p className={styles.datepost}>{date}</p>
-                </div>
-            </div>
-            <div className={styles.line}></div>
-            <p className={styles.textpost}>{children}</p>
-            <div className={styles.postfooter}>
-                <basicButton>!</basicButton>
-                <basicButton>X</basicButton>
-                <basicButton>↑</basicButton>
-                <basicButton>↕</basicButton>
-                <basicButton>↓</basicButton>
-            </div>
-        </div>
-    );
+export interface PostProps {
+  children: ReactNode;
+  title: string;
+  author: string;
+  date: string;
 }
+
+const Post: React.FC<PostProps> = ({ children, title, author, date }) => {
+  return (
+    <div className={styles.post}>
+      <div className={styles.infopost}>
+        <div className={styles.namedatepost}>
+          <p className={styles.namepost}>{author}</p>
+          <p className={styles.datepost}>{date}</p>
+        </div>
+      </div>
+      <div className={styles.line}></div>
+      <p className={styles.textpost}>{children}</p>
+      <div className={styles.postfooter}>
+        <BasicButton>!</BasicButton>
+        <BasicButton>X</BasicButton>
+        <BasicButton>↑</BasicButton>
+        <BasicButton>↕</BasicButton>
+        <BasicButton>↓</BasicButton>
+      </div>
+    </div>
+  );
+};
+
+export default Post;
