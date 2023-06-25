64e1c2d01637774bf573af79b55cc1b489fc34c5
JdM <julien@rmbi.ch>
2023-06-25 15:30:43 +0200

    modified:   src/pages/index.tsx 	modified:   src/styles/Home.module.css

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -4,12 +4,21 @@ import styles from "../styles/Home.module.css"
 export default function index() {
     return(
         <div className={styles.chatpostcom}>
-            <Post author="jdm" date="11-09-01">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus et exercitationem modi? Perferendis eaque cumque doloribus aliquam similique voluptatibus sapiente! Similique minima assumenda cupiditate iusto labore, repudiandae ullam commodi natus.
-            </Post>
-            <Post author="jdm" date="11-09-01">
-                Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus et exercitationem modi? Perferendis eaque cumque doloribus aliquam similique voluptatibus sapiente! Similique minima assumenda cupiditate iusto labore, repudiandae ullam commodi natus.
-            </Post>
+            <div className={styles.posts}>
+                <Post author="jdm" date="11-09-01">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus et exercitationem modi? Perferendis eaque cumque doloribus aliquam similique voluptatibus sapiente! Similique minima assumenda cupiditate iusto labore, repudiandae ullam commodi natus.
+                </Post>
+                <Post author="jdm" date="11-09-01">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus et exercitationem modi? Perferendis eaque cumque doloribus aliquam similique voluptatibus sapiente! Similique minima assumenda cupiditate iusto labore, repudiandae ullam commodi natus.
+                </Post>
+                <Post author="jdm" date="11-09-01">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus et exercitationem modi? Perferendis eaque cumque doloribus aliquam similique voluptatibus sapiente! Similique minima assumenda cupiditate iusto labore, repudiandae ullam commodi natus.
+                </Post>
+                <Post author="jdm" date="11-09-01">
+                    Lorem ipsum dolor sit amet consectetur adipisicing elit. Repellendus et exercitationem modi? Perferendis eaque cumque doloribus aliquam similique voluptatibus sapiente! Similique minima assumenda cupiditate iusto labore, repudiandae ullam commodi natus.
+                </Post>
+            </div>
+
         </div>
     )
 }
