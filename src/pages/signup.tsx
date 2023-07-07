2dffcb59ffa0182b74618c6036aaed9d1c80779a
JdM <julien@rmbi.ch>
2023-07-07 14:55:49 +0200

    signup

--- src/pages/signup.tsx
+++ src/pages/signup.tsx
@@ -1,37 +1,40 @@
 import styles from "../styles/Signup.module.css";
+import { useState } from "react";
 
 export default function Signup() {
-  return (
-    <div className={styles["popup-inscription"]} id="popInscript">
-      <div className={styles["popup-content"]}>
-        <h1 className={styles["connection-inscription-title"]}>Inscription</h1>
-        <div className={styles["popup-subcontent"]}>
-          <input
-            type="text"
-            className={styles["connection-inscription-input"]}
-            placeholder="Nom d'utilisateur"
-            minLength={0}
-          />
-          <input
-            type="text"
-            className={styles["connection-inscription-input"]}
-            placeholder="Email"
-            minLength={0}
-          />
-          <input
-            type="text"
-            className={styles["connection-inscription-input"]}
-            placeholder="Mot de passe"
-            minLength={8}
-          />
-          <button className={styles["connect-inscript-button"]}>
-            Inscription
-          </button>
-          <button className={styles["close-pop"]} id="closePopIns">
-            Close
-          </button>
+  
+
+    return (
+        <div className={styles["popup-inscription"]} id="popInscript">
+            <div className={styles["popup-content"]}>
+                <h1 className={styles["connection-inscription-title"]}>Inscription</h1>
+                <div className={styles["popup-subcontent"]}>
+                    <input
+                        type="text"
+                        className={styles["connection-inscription-input"]}
+                        placeholder="Nom d'utilisateur"
+                        minLength={0}
+                    />
+                    <input
+                        type="text"
+                        className={styles["connection-inscription-input"]}
+                        placeholder="Email"
+                        minLength={0}
+                    />
+                    <input
+                        type="text"
+                        className={styles["connection-inscription-input"]}
+                        placeholder="Mot de passe"
+                        minLength={8}
+                    />
+                    <button className={styles["connect-inscript-button"]}>
+                        Inscription
+                    </button>
+                    <button className={styles["close-pop"]} id="closePopIns">
+                        Close
+                    </button>
+                </div>
+            </div>
         </div>
-      </div>
-    </div>
   );
 }
