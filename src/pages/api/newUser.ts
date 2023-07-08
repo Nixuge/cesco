935bf25716dac713e528105b6e734d0871a3c545
JdM <julien@rmbi.ch>
2023-07-08 16:45:30 +0200

    new file:   src/pages/api/login.ts 	modified:   src/pages/api/newUser.ts

--- src/pages/api/newUser.ts
+++ src/pages/api/newUser.ts
@@ -5,15 +5,22 @@ import prisma from '@/lib/prisma';
 export default async function handler(req: NextApiRequest , res: NextApiResponse) {
     const { username, email, password } = req.body;
     if(username.length >= 3){
-        const result = await prisma.user.create({
-            data: {
-                username: username,
-                email: email,
-                password: password
-            }
-        })
-        res.json(result);
+        try {
+            const result = await prisma.user.create({
+                data: {
+                    username: username,
+                    email: email,
+                    password: password
+                }
+            })
+            return
+        } catch (err) {
+            res.json({ error: "A user with that username already exists 😮" })
+            return
+        }
+        
     }else{
-        res.json({ error: "Username is too short (length >= 3) " })
+        res.json({ error: "Username is too short (length must be > or = 3) 😮" })
+        return
     }
 }
