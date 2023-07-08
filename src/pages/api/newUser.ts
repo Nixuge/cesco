1bce3bd51eb56e4a66af06ff4640a28dd2f73bc4
JdM <julien@rmbi.ch>
2023-07-08 15:43:31 +0200

    too short username error

--- src/pages/api/newUser.ts
+++ src/pages/api/newUser.ts
@@ -4,13 +4,16 @@ import prisma from '@/lib/prisma';
 
 export default async function handler(req: NextApiRequest , res: NextApiResponse) {
     const { username, email, password } = req.body;
-
-    const result = await prisma.user.create({
-        data: {
-            username: username,
-            email: email,
-            password: password
-        }
-    })
-    res.json(result);
+    if(username.length >= 3){
+        const result = await prisma.user.create({
+            data: {
+                username: username,
+                email: email,
+                password: password
+            }
+        })
+        res.json(result);
+    }else{
+        res.json({ error: "Username is too short (length >= 3)" })
+    }
 }
