1c8768520820ea58e24fe507e08f25122a167328
JdM <julien@rmbi.ch>
2023-07-09 15:16:08 +0200

    api/isngin.ts

---
+++ src/pages/api/signin.ts
@@ -0,0 +1,18 @@
+import { NextApiRequest, NextApiResponse } from 'next';
+import prisma from '@/lib/prisma';
+import { useCookies } from "react-cookie"
+
+export default async function handler(req: NextApiRequest , res: NextApiResponse) {
+    const { username, password } = req.body;
+    const user = await prisma.user.findMany({
+        where: {
+            username: username,
+            password: password
+        }
+    })
+    if (user.length >= 1) {
+        res.json({accepted: true})
+    }else{
+        res.json({accepted: false})
+    }
+}
