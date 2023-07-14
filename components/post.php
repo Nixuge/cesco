08a5e196a2852284a4df39cd6211a2092352f301
JdM <julien@rmbi.ch>
2023-07-14 17:31:18 +0200

    "

--- components/post.php
+++ components/post.php
@@ -1,28 +1,27 @@
 <?php
 
-function post ($content, $author, $date){
-    return ('
-        <div class="post">
-            <div class="info-post">
-                <img src="" alt="profile.picture" class="post-profile">
-                <div class="name-date-post">
-                    <p class="name-post">{$author}</p>
-                    <p class="date-post">{$date}</p>
+function post($content, $author, $date) {
+    return "
+        <div class='post'>
+            <div class='info-post'>
+                <img src='' alt='profile.picture' class='post-profile'>
+                <div class='name-date-post'>
+                    <p class='name-post'>{$author}</p>
+                    <p class='date-post'>{$date}</p>
                 </div>
             </div>
-            <div class="line"></div>
-            <p class="text-post">{$content}</p>
+            <div class='line'></div>
+            <p class='text-post'>{$content}</p>
 
-            <div class="post-footer">
-
-                <button class="button-bottom"><p class="button-bottom-text">!</p></button>
-                <button class="button-bottom"><p class="button-bottom-text">X</p></button>
-                <button class="button-bottom"><p class="button-bottom-text">↑</p></button>
-                <button class="button-bottom"><p class="button-bottom-text">↕</p></button>
-                <button class="button-bottom"><p class="button-bottom-text">↓</p></button>
+            <div class='post-footer'>
+                <button class='button-bottom'><p class='button-bottom-text'>!</p></button>
+                <button class='button-bottom'><p class='button-bottom-text'>X</p></button>
+                <button class='button-bottom'><p class='button-bottom-text'>↑</p></button>
+                <button class='button-bottom'><p class='button-bottom-text'>↕</p></button>
+                <button class='button-bottom'><p class='button-bottom-text'>↓</p></button>
             </div>
-    </div>
-    ');
+        </div>
+    ";
 }
 
 ?>
