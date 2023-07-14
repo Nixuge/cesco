0f22ff3b173a4e4eb0f14011c6eb193073605f4b
JdM <julien@rmbi.ch>
2023-07-14 18:47:26 +0200

    v

--- src/pages/api/posts.ts
+++
@@ -1,9 +0,0 @@
-import { NextApiRequest, NextApiResponse } from 'next';
-import prisma from '@/lib/prisma';
-
-
-export default async function handler(req: NextApiRequest , res: NextApiResponse) {
-
-  const post = await prisma.post.findMany();
-  res.json(post)
-}
