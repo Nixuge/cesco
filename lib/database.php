475b96d9ff22323cd2627d9874ddd376a4dce2d9
JdM <julien@rmbi.ch>
2023-07-24 20:02:27 +0200

    ça fais 3 heur que je cherche pourquoi ça marche pas ET CT PASQUE JSPAS POURQUOI MAIS LE DEV DE PHP, qui était dépréssif et qui venai de fumer 232 join à décider que pour partager son dégout du monde en fesan chier les user IL ALLAI FAIR QUE POUR LES VARIABLE Y FAUT METTRE UN $ devant. mais après y c'est dit que ça suffisai pas ducoup il à fait que LES CONSTANTE YA PAS BESOIN  ARRGGG

--- lib/database.php
+++ lib/database.php
@@ -7,7 +7,7 @@
         private $conn;
         public function __construct() {
             include_once("../config.php");
-            $this->conn = new mysqli($DB_HOST, $DB_USERNAME, $DB_PASSWORD, $DB_NAME);
+            $this->conn = new mysqli(DB_HOST, DB_USERNAME, DB_PASSWORD, DB_NAME);
 
             if ($this->conn->connect_error) {
                 die("Connection failed: " . $this->conn->connect_error);
