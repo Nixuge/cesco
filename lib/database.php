95e4fab687fad35a86ff73865a888925028129bc
JdM <julien@rmbi.ch>
2023-07-24 18:35:25 +0200

    server_name rename in host

--- lib/database.php
+++ lib/database.php
@@ -6,7 +6,7 @@
 
         private $conn;
         public function __construct() {
-            $this->conn = new mysqli($DB_NAME, $DB_USERNAME, $DB_PASSWORD, $DB_SERVER_NAME);
+            $this->conn = new mysqli($DB_HOST, $DB_USERNAME, $DB_PASSWORD, $DB_NAME);
 
             if ($this->conn->connect_error) {
                 die("Connection failed: " . $this->conn->connect_error);
