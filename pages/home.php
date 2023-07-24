dbdad79c56aa4fe4ce682801827d287416839359
JdM <julien@rmbi.ch>
2023-07-24 16:28:38 +0200

    truc chose

--- pages/home.php
+++ pages/home.php
@@ -1,7 +1,3 @@
 <?php
-    $style = file_get_contents("style/post.css");
-    include_once("components/post.php");
-    echo post("Hello World", "JdM", "00.00.0000");
-    echo post("Hello World", "JdM", "00.00.0000");
-    echo "<style>{$style}</style>";
+
 ?>
