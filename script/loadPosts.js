1d938c60df4b6f5bb590fb3f1a0132bdcf9073d3
JdM <julien@rmbi.ch>
2023-07-24 22:03:36 +0200

    add js

---
+++ script/loadPosts.js
@@ -0,0 +1,23 @@
+function formatePosts(posts) {
+    console.log(posts)
+    let html = '';
+    posts.forEach(post => {
+        html += '<h3>${post.title}</h3>'
+    });
+
+    return html;
+}
+
+$(document).ready(function() {
+    $.ajax({
+        url: "api/posts.php",
+        type: "GET",
+        dataType: "json",
+        success: function(data) {
+            $("#theZone").html(formatePosts(data));
+        },
+        error: function() {
+            alert("Error with loading posts.");
+        }
+    });
+});
