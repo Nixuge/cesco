c3b45bd32934dd93677f8162074ae53935400ce9
JdM <julien@rmbi.ch>
2023-07-25 16:04:01 +0200

    get author posts, api

--- api/posts.php
+++ api/posts.php
@@ -6,9 +6,15 @@ include_once("../lib/database.php");
 
 $db = new Database();
 
-const getAllPostsSqlPrompt = "SELECT * FROM cesco_posts ORDER BY ID desc";
+const getAllPostsSqlPrompt = "
+SELECT cesco_posts.*, cesco_users.username AS author
+FROM cesco_posts
+JOIN cesco_users ON cesco_posts.USER_FK = cesco_users.ID
+ORDER BY cesco_posts.ID DESC;
+";
 
 $data = $db->select(getAllPostsSqlPrompt);
+
 
 echo(json_encode($data));
 ?>
