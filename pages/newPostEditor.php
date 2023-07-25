391038c09a7c1c3965404acc15206854f4772dff
JdM <julien@rmbi.ch>
2023-07-25 15:37:49 +0200

    cant post without be connected now

--- pages/newPostEditor.php
+++ pages/newPostEditor.php
@@ -1,3 +1,8 @@
+<?php
+    if(!isset($_SESSION["userId"])){
+        header("location: index.php?page=login");
+    }
+?>
 <form action="api/newPost.php" method="post">
     <textarea name="postEditorTextArea" id="postEditorTextArea" cols="30" rows="10"></textarea>
     <button type="submit">PUBLISH TO THE INTERNET</button>
