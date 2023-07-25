bb8e44167e7d71f8e93fb24b52178f254083e6a1
JdM <julien@rmbi.ch>
2023-07-25 18:01:05 +0200

    db.sql

---
+++ db.sql
@@ -0,0 +1,128 @@
+-- phpMyAdmin SQL Dump
+-- version 4.6.6deb4+deb9u2
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Jul 25, 2023 at 05:58 PM
+-- Server version: 10.1.48-MariaDB-0+deb9u2
+-- PHP Version: 5.6.40-66+freexian9.0.1+php+1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `isxk_rmbi`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cesco_users`
+--
+
+CREATE TABLE `cesco_users` (
+  `ID` int(11) NOT NULL,
+  `username` varchar(255) NOT NULL,
+  `passwd` varchar(255) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `cesco_users`
+--
+
+INSERT INTO `cesco_users` (`ID`, `username`, `passwd`) VALUES
+(0, 'Example username', 'Example password (hashed)');
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `cesco_users`
+--
+ALTER TABLE `cesco_users`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `cesco_users`
+--
+ALTER TABLE `cesco_users`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=198;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+
+-- phpMyAdmin SQL Dump
+-- version 4.6.6deb4+deb9u2
+-- https://www.phpmyadmin.net/
+--
+-- Host: localhost:3306
+-- Generation Time: Jul 25, 2023 at 05:59 PM
+-- Server version: 10.1.48-MariaDB-0+deb9u2
+-- PHP Version: 5.6.40-66+freexian9.0.1+php+1
+
+SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
+SET time_zone = "+00:00";
+
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8mb4 */;
+
+--
+-- Database: `isxk_rmbi`
+--
+
+-- --------------------------------------------------------
+
+--
+-- Table structure for table `cesco_posts`
+--
+
+CREATE TABLE `cesco_posts` (
+  `ID` int(11) NOT NULL,
+  `content` longtext NOT NULL,
+  `date` datetime NOT NULL DEFAULT CURRENT_TIMESTAMP,
+  `USER_FK` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+--
+-- Dumping data for table `cesco_posts`
+--
+
+INSERT INTO `cesco_posts` (`ID`, `content`, `date`, `USER_FK`) VALUES
+(0, 'Example post', '2023-07-25 16:11:11', 0);
+
+--
+-- Indexes for dumped tables
+--
+
+--
+-- Indexes for table `cesco_posts`
+--
+ALTER TABLE `cesco_posts`
+  ADD PRIMARY KEY (`ID`);
+
+--
+-- AUTO_INCREMENT for dumped tables
+--
+
+--
+-- AUTO_INCREMENT for table `cesco_posts`
+--
+ALTER TABLE `cesco_posts`
+  MODIFY `ID` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=270;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
