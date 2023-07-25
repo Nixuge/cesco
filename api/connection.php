d95ed24ef405d602cdbd84f85b67be53e2ca8867
JdM <julien@rmbi.ch>
2023-07-25 14:42:57 +0200

    login system test

---
+++ api/connection.php
@@ -0,0 +1,24 @@
+<?php
+session_start();
+include_once("../lib/database.php");
+include_once("../lib/hash.php");
+if (isset($_POST["username"])){
+    $password = $_POST["password"];
+    $username = $_POST["username"];
+    $hashedPassword = hashPassword($password);
+
+    $db = new Database();
+    $getUserSqlPrompt = "SELECT * FROM cesco_users WHERE username = '$username' AND password = '$hashedPassword";
+
+    $usersResult = $db->select($getUserSqlPrompt);
+    if(count($usersResult) >= 1){
+        $_SESSION['userName'] = $username;
+        $_SESSION["userId"] = $usersResult[0]['ID'];
+        header('Location: ../index.php?p=home');
+    }else{
+        $message = "Mot de passe ou nom d'utilisateur incorrect.";
+        header('Location: ../pages/signin.php?message='. urlencode($message));
+    }
+
+}
+?>
--- pages/signin.php
+++ pages/signin.php
@@ -1,6 +1,7 @@
 <div class=popup-inscription id="popInscript">
     <div class="popup-content">
         <h1 class=connection-inscription-title>Connexion</h1>
+        <h2><?php echo $_GET["message"]; ?></h2>
         <div class=popup-subcontent>
             <form action="api/connection.php" method="post">
                 <input
