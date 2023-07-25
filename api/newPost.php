597141b401e68fa5fe63a5d5dcbda519591e9511
JdM <julien@rmbi.ch>
2023-07-25 15:59:04 +0200

    userFk new post set

--- api/newPost.php
+++ api/newPost.php
@@ -3,14 +3,14 @@ session_start();
 include_once("../lib/database.php");
 
 if(isset($_POST['postEditorTextArea']) && isset($_SESSION["userId"])){
-    
+
     $db = new Database();
 
     $PostContent = $db->escapeStrings(htmlspecialchars($_POST['postEditorTextArea']));
 
-    
+    $userId = $_SESSION["userId"];
 
-    $insertNewPostSqlPrompt = "INSERT INTO cesco_posts (content, USER_FK) VALUES ('$PostContent', 0)";
+    $insertNewPostSqlPrompt = "INSERT INTO cesco_posts (content, USER_FK) VALUES ('$PostContent', $userId')";
 
     $db->query($insertNewPostSqlPrompt);
     header('Location: ../index.php?p=home');
