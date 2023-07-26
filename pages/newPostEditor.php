109782617f03f8c4fe094cba57d7ae06f54c4296
JdM <julien@rmbi.ch>
2023-07-26 18:23:04 +0200

    max post lenght fronted

--- pages/newPostEditor.php
+++ pages/newPostEditor.php
@@ -6,5 +6,23 @@ if (!isset($_SESSION["userId"])) {
 ?>
 <form action="api/newPost.php" method="post">
     <textarea name="postEditorTextArea" id="postEditorTextArea" maxlength="<?php echo MAX_POSTS_LENGTH; ?>" cols="30" rows="10"></textarea>
+    <p id="postLength"></p>
     <button type="submit">PUBLISH TO THE INTERNET</button>
 </form>
+
+<script>
+    const postLength = document.getElementById("postLength");
+    const maxlength = <?php echo MAX_POSTS_LENGTH; ?>;
+    const postEditorTextArea = document.getElementById("postEditorTextArea");
+
+    postEditorTextArea.addEventListener("input", function(e){
+        const currentLength = e.target.value.length;
+        postLength.textContent = `${currentLength} / ${maxlength}`;
+
+        if (currentLength > maxlength) {
+            postLength.style.color = "red";
+        } else {
+            postLength.style.color = "black";
+        }
+    });
+</script>
