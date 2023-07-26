ed94157cf778c9881719e5fad2bc5d7701c7c94a
JdM <julien@rmbi.ch>
2023-07-26 18:27:48 +0200

    post max length backend

--- api/newPost.php
+++ api/newPost.php
@@ -1,21 +1,21 @@
 <?php
 session_start();
 include_once("../lib/database.php");
+include_once("../config.php");
 
 if (isset($_POST['postEditorTextArea']) && isset($_SESSION["userId"])) {
+    if (count($_POST['postEditorTextArea']) > 2 && count($_POST['postEditorTextArea']) <= MAX_POSTS_LENGTH){
+        $db = new Database();
 
-    $db = new Database();
+        $PostContent = $db->escapeStrings(htmlspecialchars($_POST['postEditorTextArea']));
 
-    $PostContent = $db->escapeStrings(htmlspecialchars($_POST['postEditorTextArea']));
+        $userId = $_SESSION["userId"];
 
-    $userId = $_SESSION["userId"];
+        $insertNewPostSqlPrompt = "INSERT INTO cesco_posts (content, USER_FK) VALUES ('$PostContent', '$userId')";
 
-    $insertNewPostSqlPrompt = "INSERT INTO cesco_posts (content, USER_FK) VALUES ('$PostContent', '$userId')";
-
-    $db->query($insertNewPostSqlPrompt);
-    header('Location: ../index.php?p=home');
-
-
+        $db->query($insertNewPostSqlPrompt);
+        header('Location: ../index.php?p=home');
+    }
 }
 
 ?>
