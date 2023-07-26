c21e3272196c84f84f073ac315f781a2a8e7e6e1
JdM <julien@rmbi.ch>
2023-07-26 18:07:54 +0200

    max post length

--- pages/newPostEditor.php
+++ pages/newPostEditor.php
@@ -1,9 +1,10 @@
 <?php
+include_once("config.php");
 if (!isset($_SESSION["userId"])) {
     header("location: index.php?p=signin");
 }
 ?>
 <form action="api/newPost.php" method="post">
-    <textarea name="postEditorTextArea" id="postEditorTextArea" cols="30" rows="10"></textarea>
+    <textarea name="postEditorTextArea" id="postEditorTextArea" maxlength="<?php echo MAX_POSTS_LENGTH; ?>" cols="30" rows="10"></textarea>
     <button type="submit">PUBLISH TO THE INTERNET</button>
 </form>
