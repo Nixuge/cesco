bf2efe08413540e9b4c8107c8476f64a578a9631
JdM <julien@rmbi.ch>
2023-08-07 19:42:39 +0200

    pff

--- api/posts.php
+++ api/posts.php
@@ -5,9 +5,11 @@ include_once("../lib/database.php");
 
 $db = new Database();
 
-const getAllPostsSqlPrompt = "
+const getAllPostsSql = "
 SELECT cesco_posts.*, cesco_users.username AS author, 
-       COUNT(cesco_votes.POST_FK) AS votes_count
+    SUM(cesco_votes.vote_type = 2) AS votes_positives_count,
+    SUM(cesco_votes.vote_type = 1) AS votes_neutrals_count,
+    SUM(cesco_votes.vote_type = 0) AS votes_negatives_count
 FROM cesco_posts
 JOIN cesco_users ON cesco_posts.USER_FK = cesco_users.ID
 LEFT JOIN cesco_votes ON cesco_posts.ID = cesco_votes.POST_FK
@@ -15,7 +17,7 @@ GROUP BY cesco_posts.ID
 ORDER BY cesco_posts.ID DESC;
 ";
 
-$data = $db->select(getAllPostsSqlPrompt);
+$data = $db->select(getAllPostsSql);
 
-echo (json_encode($data));
+echo json_encode($data);
 ?>
