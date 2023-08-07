caa64dfdc64ba592ba3d0e165014c29ef1d8139f
JdM <julien@rmbi.ch>
2023-08-07 18:01:43 +0200

    f

--- api/posts.php
+++ api/posts.php
@@ -1,5 +1,4 @@
 <?php
-
 header('Content-Type: application/json; charset=utf-8');
 
 include_once("../lib/database.php");
@@ -7,14 +6,16 @@ include_once("../lib/database.php");
 $db = new Database();
 
 const getAllPostsSqlPrompt = "
-SELECT cesco_posts.*, cesco_users.username AS author
+SELECT cesco_posts.*, cesco_users.username AS author, 
+       COUNT(cesco_votes.ID) AS votes_count
 FROM cesco_posts
 JOIN cesco_users ON cesco_posts.USER_FK = cesco_users.ID
+LEFT JOIN cesco_votes ON cesco_posts.ID = cesco_votes.POST_FK
+GROUP BY cesco_posts.ID
 ORDER BY cesco_posts.ID DESC;
 ";
 
 $data = $db->select(getAllPostsSqlPrompt);
-
 
 echo (json_encode($data));
 ?>
