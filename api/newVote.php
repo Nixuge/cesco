cab64553664792828ae16ed6adb4752e51afc140
JdM <julien@rmbi.ch>
2023-08-07 15:57:39 +0200

    musst be loged in to vote

--- api/newVote.php
+++ api/newVote.php
@@ -3,14 +3,18 @@ session_start();
 include_once("../lib/database.php");
 
 if(isset($_GET["post_id"]) && isset($_GET["type"]) && $_GET["type"] <= 2 && $_GET["type"] >= 0 && is_int($_GET["post_id"]) && is_int($_GET["type"])){
-    $db = new Database();
-    $userId = $_SESSION["userId"];
-    $voteType = $_GET["type"];
-    $postId = $_GET["post_id"];
+    if(isset($_SESSION["userId"])){
+        $db = new Database();
+        $userId = $_SESSION["userId"];
+        $voteType = $_GET["type"];
+        $postId = $_GET["post_id"];
 
-    $sqlQuery = "INSERT INTO cesco_votes (vote_type, USER_FK, POST_FK) VALUES ('$voteType', '$userId', '$postId')";
-    
-    $db->query($sqlQuery);
+        $sqlQuery = "INSERT INTO cesco_votes (vote_type, USER_FK, POST_FK) VALUES ('$voteType', '$userId', '$postId')";
+
+        $db->query($sqlQuery);
+    }else{
+        header("location: index.php?p=signin");
+    }
 }else{
     echo "Parameters error";
 }
