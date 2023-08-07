cde14976e5a8732ceb3580c06fc1a324438e6c6b
JdM <julien@rmbi.ch>
2023-08-07 20:04:36 +0200

    iffnull "0"

--- api/posts.php
+++ api/posts.php
@@ -7,9 +7,9 @@ $db = new Database();
 
 const getAllPostsSql = "
 SELECT cesco_posts.*, cesco_users.username AS author, 
-    SUM(cesco_votes.vote_type = 2) AS votes_positives_count,
-    SUM(cesco_votes.vote_type = 1) AS votes_neutrals_count,
-    SUM(cesco_votes.vote_type = 0) AS votes_negatives_count
+       IFNULL(SUM(cesco_votes.vote_type = 2), 0) AS votes_positives_count,
+       IFNULL(SUM(cesco_votes.vote_type = 1), 0) AS votes_neutrals_count,
+       IFNULL(SUM(cesco_votes.vote_type = 0), 0) AS votes_negatives_count
 FROM cesco_posts
 JOIN cesco_users ON cesco_posts.USER_FK = cesco_users.ID
 LEFT JOIN cesco_votes ON cesco_posts.ID = cesco_votes.POST_FK
