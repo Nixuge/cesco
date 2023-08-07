f30a4a8db9ed485904ebd01e6e2b5847e0e75678
JdM <julien@rmbi.ch>
2023-08-07 20:20:49 +0200

    delte if arleady liked

--- api/newVote.php
+++ api/newVote.php
@@ -9,12 +9,14 @@ if(isset($_POST["post_id"]) && isset($_POST["type"]) && (int)$_POST["type"] <= 2
         $voteType = (int)$_POST["type"];
         $postId = (int)$_POST["post_id"];
 
-        $verifyIfAlreadyLikedSqlQuery = "SELECT * FROM cesco_votes WHERE USER_FK = '$userId' AND POST_FK='$postId'";
+        $verifyIfAlreadyLikedSqlQuery = "SELECT * FROM cesco_votes WHERE USER_FK = '$userId' AND POST_FK='$postId' AND vote_type = '$voteType'";
         $verifyIfAlreadyLikedResult = $db->select($verifyIfAlreadyLikedSqlQuery);
 
         if(count($verifyIfAlreadyLikedResult) == 0){
             $insertVoteSqlQuery = "INSERT INTO cesco_votes (vote_type, USER_FK, POST_FK) VALUES ('$voteType', '$userId', '$postId')";
             $db->query($insertVoteSqlQuery);
+        }else{
+            $removeVoteSqlQuery = "DELETE FROM cesco_votes WHERE USER_FK = '$userId' AND POST_FK='$postId' AND vote_type = '$voteType'";
         }
 
     }else{
