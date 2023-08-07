ad44e30bcb2197323c3b31133b5584ce910d4577
JdM <julien@rmbi.ch>
2023-08-07 19:21:29 +0200

    fix

--- api/posts.php
+++ api/posts.php
@@ -7,7 +7,7 @@ $db = new Database();
 
 const getAllPostsSqlPrompt = "
 SELECT cesco_posts.*, cesco_users.username AS author, 
-       COUNT(cesco_votes.ID) AS votes_count
+       COUNT(cesco_votes.POST_FK) AS votes_count
 FROM cesco_posts
 JOIN cesco_users ON cesco_posts.USER_FK = cesco_users.ID
 LEFT JOIN cesco_votes ON cesco_posts.ID = cesco_votes.POST_FK
