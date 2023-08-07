cc6bc7a0e8e41f0127184295f329c945bd66fef2
JdM <julien@rmbi.ch>
2023-08-07 19:59:42 +0200

    vote count in another place

--- script/loadPosts.js
+++ script/loadPosts.js
@@ -14,9 +14,9 @@ function formatePost(content, author, date, ID, positives_votes, neutral_votes, 
             <div class='left-post'>
                 <img src='images/example.png' alt='profile.picture' class='post-profile'>
                 <div class='post-buttons'>
-                    <button onclick='vote(${ID}, 2)' class='action-button up-action-button'><p class='action-button-text'>↑</p></button><p>${positives_votes}</p>
-                    <button onclick='vote(${ID}, 1)' class='action-button multi-action-button'><p class='action-button-text'>↕</p></button><p>${neutral_votes}</p>
-                    <button onclick='vote(${ID}, 0)' class='action-button down-action-button'><p class='action-button-text'>↓</p></button><p>${negative_votes}</p>
+                    <button onclick='vote(${ID}, 2)' class='action-button up-action-button'><p class='action-button-text'>↑</p></button>
+                    <button onclick='vote(${ID}, 1)' class='action-button multi-action-button'><p class='action-button-text'>↕</p>
+                    <button onclick='vote(${ID}, 0)' class='action-button down-action-button'><p class='action-button-text'>↓</p></button>
                     <button class='action-button warn-action-button'><p class='action-button-text'>!</p></button>
                     <button class='action-button del-action-button'><p class='action-button-text'>X</p></button>
                 </div>
@@ -27,6 +27,9 @@ function formatePost(content, author, date, ID, positives_votes, neutral_votes, 
                     <p class='separator-dash'>-</p>
                     <p class='date-post'>${date}</p>
                 </div>
+                <p>↑ ${positives_votes}</p>
+                <p>↕ ${neutral_votes}</p>
+                <p>↓ ${negative_votes}</p>
                 <p class='text-post'>${content}</p>
             </div>
         </div>
