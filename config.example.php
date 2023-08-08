4b6b8445ae5390ce2777e82956606256ac953416
JdM <julien@rmbi.ch>
2023-08-08 12:41:49 +0200

    update config to add banneds username words

--- config.example.php
+++ config.example.php
@@ -4,7 +4,9 @@ define("DB_NAME", "Your database name");
 define("DB_USERNAME", "Your database username");
 define("DB_PASSWORD", "Your database password");
 
-define("HASH_SECRET", "A random string (not very important)");
+define("HASH_SECRET", "A random string (not very important for tests)");
 
 define('MAX_POSTS_LENGTH', 500);
+
+define("BANNED_WORDS_USERNAMES", array("admin", "modo", "moderateur", "moderator", "founder", "creator", "official", "officiel", "verified", "vérifié"));
 ?>
