081c8eaaab636d74665f7627739614f2247bb31d
JdM <julien@rmbi.ch>
2023-08-08 14:28:23 +0200

    redirect to https INDEX

--- index.php
+++ index.php
@@ -16,6 +16,16 @@
     </header>
 
     <?php
+
+    // redirect to https
+    if (empty($_SERVER['HTTPS']) || $_SERVER['HTTPS'] === "off") {
+        $location = 'https://' . $_SERVER['HTTP_HOST'] . $_SERVER['REQUEST_URI'];
+        header('HTTP/1.1 301 Moved Permanently');
+        header('Location: ' . $location);
+        exit;
+    }
+
+    
     session_start();
 
     $page = $_GET["p"];
