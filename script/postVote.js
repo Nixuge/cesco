80d2a500808129f1b1dc6b3fa26d4dde7c8e3456
JdM <julien@rmbi.ch>
2023-08-08 12:09:56 +0200

    update post vote count after voting

--- script/postVote.js
+++ script/postVote.js
@@ -1,8 +1,13 @@
-function vote(postId, vote_type){
-    $.post("api/newVote.php",
-        {
-            post_id: postId,
-            type: vote_type
-        }
-    )
+function vote(postId, vote_type) {
+    $.post("api/newVote.php", {
+        post_id: postId,
+        type: vote_type
+    })
+    .done(function(response) {
+        updatePost(postId)
+    })
+    .fail(function(jqXHR, textStatus, errorThrown) {
+        console.error("Vote failed:", errorThrown); 
+        alert("An error occurred while voting. Please try again later."); 
+    });
 }
