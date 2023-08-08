b21476b2a73467931953c8f79b947d61f786bc93
JdM <julien@rmbi.ch>
2023-08-08 12:54:24 +0200

    `"banned_words_usernames"=>BANNED_WORDS_USERNAMES`

--- api/getConfig.php
+++ api/getConfig.php
@@ -2,7 +2,7 @@
 include_once("../config.php");
 header('Content-Type: application/json; charset=utf-8');
 
-$data = array("max_posts_length" => MAX_POSTS_LENGTH);
+$data = array("max_posts_length" => MAX_POSTS_LENGTH, "banned_words_usernames" => BANNED_WORDS_USERNAMES);
 
 echo json_encode($data);
 ?>
