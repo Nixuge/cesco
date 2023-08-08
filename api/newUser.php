97635f37da9041e1424adf56f1575c86bc19b6b3
JdM <julien@rmbi.ch>
2023-08-08 13:33:15 +0200

    Modifications apportées :
        Ajout d'une vérification pour le cas où la session existe déjà ou l'utilisateur est déjà connecté.
        Ajout d'une condition pour vérifier si la sélection d'un utilisateur existant a réussi avant d'extraire l'ID de l'utilisateur.
        Ajout d'une redirection vers une page d'erreur en cas de problèmes imprévus.
        Améliorations de la mise en forme pour une meilleure lisibilité.
        Utilisation de empty() pour vérifier si le tableau $userDbInfo est
        vide.

--- api/newUser.php
+++ api/newUser.php
@@ -6,40 +6,44 @@ include_once("../lib/database.php");
 include_once("../lib/hash.php");
 include_once("../lib/containBannedWord.php");
 include_once("../config.php");
+
 if (isset($_POST["username"]) && !isset($_SESSION["userId"])) {
     $db = new Database();
 
     $password = $_POST["password"];
     $username = $db->escapeStrings(htmlspecialchars($_POST["username"]));
     $hashedPassword = $db->escapeStrings(hashPassword($password));
-    print_r($BANNED_WORDS_USERNAMES);
-    if(!containBannedWord($BANNED_WORDS_USERNAMES, $username)){
+
+    if (!containBannedWord(BANNED_WORDS_USERNAMES, $username)) {
         $checkIfUsernameIsAlreadyTakenSqlQuery = "SELECT username FROM cesco_users WHERE username = '$username'";
         $existingUsernames = $db->select($checkIfUsernameIsAlreadyTakenSqlQuery);
-    
-        if(count($existingUsernames) <= 0){
-    
-            $insertNewUserSqlPrompt = "INSERT INTO cesco_users (username,passwd) VALUES ('$username','$hashedPassword')";
+
+        if (count($existingUsernames) <= 0) {
+            $insertNewUserSqlPrompt = "INSERT INTO cesco_users (username, passwd) VALUES ('$username', '$hashedPassword')";
             $db->query($insertNewUserSqlPrompt);
-        
-            $getUserIdSqlPrompt = "SELECT * FROM cesco_users WHERE username = '$username' AND passwd = '$hashedPassword'";
+
+            $getUserIdSqlPrompt = "SELECT ID FROM cesco_users WHERE username = '$username' AND passwd = '$hashedPassword'";
             $userDbInfo = $db->select($getUserIdSqlPrompt);
-        
-            $userID = $userDbInfo[0]["ID"];
-            $_SESSION['userID'] = $db->escapeStrings($userID);
-            $_SESSION['userName'] = $db->escapeStrings(htmlspecialchars($username));
-            header('Location: ../index.php?p=home');
-        }else{
-            $message = "Ce nom d'utilisateur est dêja pris, veuillez en utiliser un autre.";
+
+            if (!empty($userDbInfo)) {
+                $userID = $userDbInfo[0]["ID"];
+                $_SESSION['userID'] = $db->escapeStrings($userID);
+                $_SESSION['userName'] = $db->escapeStrings(htmlspecialchars($username));
+                header('Location: ../index.php?p=home');
+            } else {
+                $message = "Une erreur s'est produite lors de la création du compte.";
+                header('Location: ../index.php?p=signup&message=' . urlencode($message));
+            }
+        } else {
+            $message = "Ce nom d'utilisateur est déjà pris, veuillez en utiliser un autre.";
             header('Location: ../index.php?p=signup&message=' . urlencode($message));
         }
-    
-    }else{
-        $message = "Veulliez utiliser un autre nom d'utilisateur";
+    } else {
+        $message = "Veuillez utiliser un autre nom d'utilisateur.";
         header('Location: ../index.php?p=signup&message=' . urlencode($message));
     }
-
-
-
+} else {
+    $message = "Une erreur s'est produite.";
+    header('Location: ../index.php?p=signup&message=' . urlencode($message));
 }
 ?>
