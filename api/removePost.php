095bd2e87495b6ad72445009e1700342f1782587
JdM <julien@rmbi.ch>
2023-08-09 17:05:36 +0200

    removePost moderation

---
+++ api/removePost.php
@@ -0,0 +1,21 @@
+<?php
+include_once("../lib/database.php");
+session_start();
+
+define("MODERATOR_GRADES", array("admin", "founder", "moderator"));
+
+if (isset($_GET["post_id"])){
+
+    $db = new Database();
+
+    $postId = $db->escapeStrings((int)$_GET["post_id"]);
+    $userId = $_SESSION["useId"];
+    $getGradeSqlQuery = "SELECT grade FROM cesco_users WHERE ID = $userId";
+    $grade = $db->select($getGradeSqlQuery)[0]["grade"];
+
+    if(in_array($grad, MODERATOR_GRADES)){
+        $deletePostSqlQuery = "DELETE FROM cesco_posts WHERE ID = $postId";
+        $db->query($deletePostSqlQuery);
+    }
+}
+?>
