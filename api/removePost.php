8d2dacabb759a0748da72951cd8d474028a0f5fe
JdM <julien@rmbi.ch>
2023-08-09 17:09:11 +0200

    forget ' in the sql query [fix]

--- api/removePost.php
+++ api/removePost.php
@@ -10,11 +10,11 @@ if (isset($_GET["post_id"]) && isset($_SESSION["userId"])){
 
     $postId = $db->escapeStrings((int)$_GET["post_id"]);
     $userId = $_SESSION["useId"];
-    $getGradeSqlQuery = "SELECT grade FROM cesco_users WHERE ID = $userId";
+    $getGradeSqlQuery = "SELECT grade FROM cesco_users WHERE ID = '$userId'";
     $grade = $db->select($getGradeSqlQuery)[0]["grade"];
 
     if(in_array($grad, MODERATOR_GRADES)){
-        $deletePostSqlQuery = "DELETE FROM cesco_posts WHERE ID = $postId";
+        $deletePostSqlQuery = "DELETE FROM cesco_posts WHERE ID = '$postId'";
         $db->query($deletePostSqlQuery);
     }
 }
