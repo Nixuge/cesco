5f05803e50b4fd4a1a985858705e07634437a44c
JdM <julien@rmbi.ch>
2023-08-09 17:21:29 +0200

    add to config

--- config.example.php
+++ config.example.php
@@ -8,5 +8,7 @@ define("HASH_SECRET", "A random string (not very important for tests)");
 
 define('MAX_POSTS_LENGTH', 500);
 
-define("BANNED_WORDS_USERNAMES", array("admin", "modo", "moderateur", "moderator", "founder", "creator", "official", "officiel", "verified", "vérifié"));
+define("BANNED_WORDS_USERNAMES", array("admin", "modo", "moderateur", "moderator", "founder", "creator", "official", "officiel", "verified", "vérifié", "asteroidus"));
+
+define("MODERATOR_GRADES", array("admin", "founder", "moderator"));
 ?>
