679ad960032b153269681d4fc28b296ca6999a04
JdM <julien@rmbi.ch>
2023-08-10 16:24:57 +0200

    add number of report

--- api/report.php
+++ api/report.php
@@ -2,13 +2,23 @@
 session_start();
 
 include_once("utils/database.php");
-$post_id = $db ->escapeStrings($_POST["post_id"]);
-$reason = $db ->escapeStrings($_POST["reason"]);
-$userPK = $db ->escapeStrings($_SESSION["userPk"]);
 $db = new Database();
 
-$insertReportSqlQuery = "INSERT INTO cesco_reports (POST_FK, REPORTER_FK, reason) VALUES ('$post_id', '$userPk', '$reason')";
-$db ->query($insertReportSqlQuery);
+$post_id = $db->escapeStrings($_POST["post_id"]);
+$reason = $db->escapeStrings($_POST["reason"]);
+$userPK = $db->escapeStrings($_SESSION["userPk"]);
 
+// Check if the post has already been reported by the same user
+$checkReportSqlQuery = "SELECT * FROM cesco_reports WHERE POST_FK = '$post_id'";
+$result = $db->query($checkReportSqlQuery);
 
+if ($result->num_rows > 0) {
+    // Update the 'count' column
+    $updateCountSqlQuery = "UPDATE cesco_reports SET count = count + 1 WHERE POST_FK = '$post_id' AND REPORTER_FK = '$userPK'";
+    $db->query($updateCountSqlQuery);
+} else {
+    // Insert a new report
+    $insertReportSqlQuery = "INSERT INTO cesco_reports (POST_FK, REPORTER_FK, reason, count) VALUES ('$post_id', '$userPK', '$reason', 0)";
+    $db->query($insertReportSqlQuery);
+}
 ?>
