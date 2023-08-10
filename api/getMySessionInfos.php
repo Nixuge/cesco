b28bf85a2438683699af79572bb2f34b7c2598ea
JdM <julien@rmbi.ch>
2023-08-10 10:04:17 +0200

    type error

--- api/getMySessionInfos.php
+++ api/getMySessionInfos.php
@@ -3,7 +3,7 @@ session_start();
 header('Content-Type: application/json');
 $userId = $_SESSION["userId"];
 $userName = $_SESSION["userName"];
-$$userGrade = $_SESSION["userGrade"];
+$userGrade = $_SESSION["userGrade"];
 $infoArray = ["userId"=>$userId, "userName"=>$userName, "userGrade"=>$userGrade];
 echo json_encode($infoArray);
 ?>
