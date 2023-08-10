eeadec7af0a0b610e66e40404f03b6c700827bd7
JdM <julien@rmbi.ch>
2023-08-10 16:20:20 +0200

    esacpe string report.php

--- api/report.php
+++ api/report.php
@@ -2,9 +2,9 @@
 session_start();
 
 include_once("utils/database.php");
-$post_id = $_POST["post_id"];
-$reason = $_POST["reason"];
-$userPK = $_SESSION["userPk"];
+$post_id = $db ->escapeStrings($_POST["post_id"]);
+$reason = $db ->escapeStrings($_POST["reason"]);
+$userPK = $db ->escapeStrings($_SESSION["userPk"]);
 $db = new Database();
 
 $insertReportSqlQuery = "INSERT INTO cesco_reports (POST_FK, REPORTER_FK, reason) VALUES ('$post_id', '$userPk', '$reason')";
