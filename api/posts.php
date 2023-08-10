f9148e5cc181118637280d2f5dd1b31203044b02
Nixuge <mail@nixuge.me>
2023-08-10 12:14:27 +0200

    refactor posts.php

--- api/posts.php
+++ api/posts.php
@@ -5,15 +5,18 @@ include_once("utils/database.php");
 
 $db = new Database();
 
-if(!isset($_GET["id"])){
-    $getAllPostsSql = "
+$baseSqlQuery = "
     SELECT cesco_posts.*, cesco_users.username AS author, 
-        IFNULL(SUM(cesco_votes.vote_type = 2), 0) AS votes_positives_count,
-        IFNULL(SUM(cesco_votes.vote_type = 1), 0) AS votes_neutrals_count,
-        IFNULL(SUM(cesco_votes.vote_type = 0), 0) AS votes_negatives_count
+    IFNULL(SUM(cesco_votes.vote_type = 2), 0) AS votes_positives_count,
+    IFNULL(SUM(cesco_votes.vote_type = 1), 0) AS votes_neutrals_count,
+    IFNULL(SUM(cesco_votes.vote_type = 0), 0) AS votes_negatives_count
     FROM cesco_posts
     JOIN cesco_users ON cesco_posts.USER_FK = cesco_users.ID
     LEFT JOIN cesco_votes ON cesco_posts.ID = cesco_votes.POST_FK
+    ";
+
+if (!isset($_GET["id"])) {
+    $getAllPostsSql = $baseSqlQuery + "
     GROUP BY cesco_posts.ID
     ORDER BY cesco_posts.ID DESC;
     ";
@@ -22,17 +25,10 @@ if(!isset($_GET["id"])){
 
     echo json_encode($data);
 
-}else{
+} else {
     $postID = $db->escapeStrings($_GET["id"]);
 
-    $getPostByIdSql = "
-    SELECT cesco_posts.*, cesco_users.username AS author, 
-        IFNULL(SUM(cesco_votes.vote_type = 2), 0) AS votes_positives_count,
-        IFNULL(SUM(cesco_votes.vote_type = 1), 0) AS votes_neutrals_count,
-        IFNULL(SUM(cesco_votes.vote_type = 0), 0) AS votes_negatives_count
-    FROM cesco_posts
-    JOIN cesco_users ON cesco_posts.USER_FK = cesco_users.ID
-    LEFT JOIN cesco_votes ON cesco_posts.ID = cesco_votes.POST_FK
+    $getPostByIdSql = $baseSqlQuery + "
     WHERE cesco_posts.ID = '$postID'
     GROUP BY cesco_posts.ID;
     ";
