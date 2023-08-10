1425830f4e6db9ef2385bf326515a03c128aa683
JdM <julien@rmbi.ch>
2023-08-10 16:33:16 +0200

    report fronted

--- script/report.js
+++ script/report.js
@@ -1,3 +1,16 @@
-function report(post_id){
-    prompt("Quel est le problème ?");
+function report(postid){
+    const reasonUser = prompt("Quel est le problème ?");
+
+    $.post("api/report.php", {
+        post_id: postid,
+        reason: reasonUser
+    })
+    .done(function(response) {
+        alert("Votre signalement à bien été reçu. Merci.")
+    })
+    .fail(function(jqXHR, textStatus, errorThrown) {
+        console.error("Vote failed:", errorThrown); 
+        alert("An error occurred while voting. Please try again later."); 
+    });
+
 }
