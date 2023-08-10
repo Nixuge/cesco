b0170b9ae1fbe8abfbd4493c3bd395b117096386
Nixuge <mail@nixuge.me>
2023-08-10 12:20:45 +0200

    Refactor connection.php

--- api/connection.php
+++ api/connection.php
@@ -2,26 +2,29 @@
 session_start();
 include_once("utils/database.php");
 include_once("utils//hash.php");
-if (isset($_POST["username"])) {
-    $db = new Database();
 
-    $password = $_POST["password"];
-    $username = $db->escapeStrings(htmlspecialchars($_POST["username"]));
-    $hashedPassword = $db->escapeStrings(hashPassword($password));
-
-
-    $getUserSqlPrompt = "SELECT ID, grade FROM cesco_users WHERE username = '$username' AND passwd = '$hashedPassword'";
-
-    $usersResult = $db->select($getUserSqlPrompt);
-    if (count($usersResult) >= 1) {
-        $_SESSION['userName'] = $db->escapeStrings(htmlspecialchars($username));
-        $_SESSION["userId"] = $db->escapeStrings($usersResult[0]['ID']);
-        $_SESSION["userGrade"] = $db->escapeStrings($usersResult[0]['grade']);
-        header('Location: ../index.php?p=home');
-    } else {
-        $message = "Mot de passe ou nom d'utilisateur incorrect.";
-        header('Location: ../index.php?p=signin&message=' . urlencode($message));
-    }
-
+if (!isset($_POST["username"])) {
+    exit();
 }
+
+$db = new Database();
+
+$password = $_POST["password"];
+$username = $db->escapeStrings(htmlspecialchars($_POST["username"]));
+$hashedPassword = $db->escapeStrings(hashPassword($password));
+
+$getUserSqlPrompt = "SELECT ID, grade FROM cesco_users WHERE username = '$username' AND passwd = '$hashedPassword'";
+
+$usersResult = $db->select($getUserSqlPrompt);
+
+if (count($usersResult) == 0) {
+    $message = "Mot de passe ou nom d'utilisateur incorrect.";
+    header('Location: ../index.php?p=signin&message=' . urlencode($message));
+}
+
+$_SESSION['userName'] = $db->escapeStrings(htmlspecialchars($username));
+$_SESSION["userId"] = $db->escapeStrings($usersResult[0]['ID']);
+$_SESSION["userGrade"] = $db->escapeStrings($usersResult[0]['grade']);
+header('Location: ../index.php?p=home');
+
 ?>
