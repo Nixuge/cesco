98d65cd4636fc7f51706cf2eff23d13acf7207b8
Nixuge <mail@nixuge.me>
2023-08-10 12:17:50 +0200

    Format newVote.php

--- api/newVote.php
+++ api/newVote.php
@@ -2,19 +2,19 @@
 session_start();
 include_once("utils/database.php");
 
-if(!(isset($_POST["post_id"]) && isset($_POST["type"]) && (int)$_POST["type"] <= 2 && (int)$_POST["type"] >= 0 )) {
+if (!(isset($_POST["post_id"]) && isset($_POST["type"]) && (int) $_POST["type"] <= 2 && (int) $_POST["type"] >= 0)) {
     echo "Parameters error";
     exit();
 }
-if(!isset($_SESSION["userId"])) {
+if (!isset($_SESSION["userId"])) {
     header("location: ../index.php?p=signin");
     exit();
 }
 
 $db = new Database();
 $userId = $db->escapeStrings($_SESSION["userId"]);
-$voteType = (int)$_POST["type"];
-$postId = (int)$_POST["post_id"];
+$voteType = (int) $_POST["type"];
+$postId = (int) $_POST["post_id"];
 
 $verifyIfAlreadyLikedSqlQuery = "SELECT * FROM cesco_votes WHERE USER_FK = '$userId' AND POST_FK='$postId' AND vote_type = '$voteType'";
 $verifyIfAlreadyLikedResult = $db->select($verifyIfAlreadyLikedSqlQuery);
